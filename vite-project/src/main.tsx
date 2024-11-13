73b282e8cc78782b5fc4783dae0afdad8fa560ab
Netanel Rotenstein <email@example.com>
2024-11-13 16:15:04 +0200

    main

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+
+export const socket = io(`http://localhost:1212`)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider >
+        <App />
+      </Provider >
+    </BrowserRouter>
   </StrictMode>,
 )
