72619d9528d51e6af115011dbbec4068b77c559d
Netanel Rotenstein <email@example.com>
2024-11-14 14:17:26 +0200

    adding area

--- vite-project/src/componnents/Attack.tsx
+++ vite-project/src/componnents/Attack.tsx
@@ -2,24 +2,26 @@ import React, { useEffect, useState } from 'react'
 import { socket } from '../main';
 import { useAppSelector } from '../redux/store';
 import { io } from 'socket.io-client';
+import { areaEnum } from '../types/areaEnum';
 
 export default function Attack() {
     const { user } = useAppSelector((state) => state.user)
+    const [areaAttack, setareaAttack] = useState<areaEnum>()
     let [mymissiles, setmymissiles] = useState<any[]>([1])
     const [historyAttack, sethistoryAttack] = useState<any[]>([])
     useEffect(() => {
         const data = user?._id
-        socket.emit("iAmConnected", data);
+        socket.emit("iAmConnectedAttack", data);
     }, []);
 
-    socket.on('relevantMissiles', async(data) => {
+    socket.on('relevantMissiles', async (data) => {
         await setmymissiles(data)
     })
-    socket.on("historyAttack",async(data)=>{
+    socket.on("historyAttack", async (data) => {
         await sethistoryAttack(data)
     })
-    const shutingDefence = (user_id: string | undefined, bumName: string) => {
-        const data = { user_id, bumName }
+    const shutingAttack = (user_id: string | undefined, bumName: string, area?: areaEnum) => {
+        const data = { user_id, bumName,area }
         socket.emit('shutingAttack', data)
         socket.emit("iAmConnected", user_id);
     }
@@ -27,10 +29,18 @@ export default function Attack() {
         <div>
             Your weapon:{mymissiles.map((msl: any) =>
                 <div>name:{msl.name} , amount:{msl.amount}
-                    {msl.amount > 0 ? <button onClick={() => shutingDefence(user?._id, msl.name)}>Shut</button> : <div></div>}
+                    <select value={areaAttack}
+                    onChange={(e) => setareaAttack(e.target.value as areaEnum)}>
+
+                        <option value={areaEnum.Center}>Center</option>
+                        <option value={areaEnum.South}>South</option>
+                        <option value={areaEnum.North}>North</option>
+                        <option value={areaEnum.West}>West</option>
+                    </select>
+                    {msl.amount > 0 ? <button onClick={() => shutingAttack(user?._id, msl.name,areaAttack)}>Shut</button> : <div></div>}
                 </div>)}
-                <table>{history.map(h => <td>bum name:{h.missile} success:{h.success} </td>)}</table>
+            <table>{historyAttack.map(h => <td>bum name:{h.missile} </td>)}</table>
         </div>
-        
+
     )
 }
--- vite-project/src/componnents/Defence.tsx
+++ vite-project/src/componnents/Defence.tsx
@@ -2,40 +2,41 @@ import React, { useEffect, useState } from 'react'
 import { socket } from '../main';
 import { useAppSelector } from '../redux/store';
 import { io } from 'socket.io-client';
+import { areaEnum } from '../types/areaEnum';
 
 export default function Defence() {
     const { user } = useAppSelector((state) => state.user)
     let [mymissiles, setmymissiles] = useState<any[]>([1])
     const [history, sethistory] = useState<any[]>([])
     useEffect(() => {
-        const data = {user_id:user?._id,area:user?.area}
+        const data = { user_id: user?._id, area: user?.area }
         socket.emit("iAmConnectedDefence", data);
     }, []);
 
-    socket.on('relevantMissiles', async(data) => {
+    socket.on('relevantMissiles', async (data) => {
         await setmymissiles(data)
     })
-    socket.on("historyDefence",async(data)=>{
+    socket.on("historyDefence", async (data) => {
         await sethistory(data)
     })
-    const shutingDefence = (user_id: string | undefined, bumName: string) => {
-        const data = { user_id, bumName }
+    const shutingDefence = (user_id: string | undefined, bumName: string, area?: areaEnum | undefined) => {
+        const data = { user_id, bumName, area }
         socket.emit('shutingDefence', data)
-        socket.emit("iAmConnectedDefence", {user_id,area:user?.area});
+        socket.emit("iAmConnectedDefence", { user_id, area: user?.area });
     }
     return (
         <div>
             Your weapon:{mymissiles.map((msl: any) =>
                 <div>name:{msl.name} , amount:{msl.amount}
-                    {msl.amount > 0 ? <button onClick={() => shutingDefence(user?._id, msl.name)}>Shut</button> : <div></div>}
+                    {msl.amount > 0 ? <button onClick={() => shutingDefence(user?._id, msl.name, user?.area)}>Shut</button> : <div></div>}
                 </div>)}
 
-                Your history:{history.map((h: any) =>
+            Your history:{history.map((h: any) =>
                 <div>
                     bum name:{h.missile} , success:{h.success}
                 </div>)}
 
         </div>
-        
+
     )
 }
